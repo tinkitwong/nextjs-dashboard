2b4ad5f1170be395789beb97ff40bfad70d0304a
Tin Kit <tinkitwong@gmail.com>
2024-07-07 15:10:43 +0800

    chore: update progress - chapter 11 done

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,6 +1,8 @@
-// import bcrypt from 'bcrypt';
-// import { db } from '@vercel/postgres';
-// import { invoices, customers, revenue, users } from '../lib/placeholder-data';
+// Database has been seeded once.
+
+// import bcrypt from "bcrypt";
+// import { db } from "@vercel/postgres";
+// import { invoices, customers, revenue, users } from "../lib/placeholder-data";
 
 // const client = await db.connect();
 
@@ -23,7 +25,7 @@
 //         VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
 //         ON CONFLICT (id) DO NOTHING;
 //       `;
-//     }),
+//     })
 //   );
 
 //   return insertedUsers;
@@ -48,8 +50,8 @@
 //         INSERT INTO invoices (customer_id, amount, status, date)
 //         VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
 //         ON CONFLICT (id) DO NOTHING;
-//       `,
-//     ),
+//       `
+//     )
 //   );
 
 //   return insertedInvoices;
@@ -73,8 +75,8 @@
 //         INSERT INTO customers (id, name, email, image_url)
 //         VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
 //         ON CONFLICT (id) DO NOTHING;
-//       `,
-//     ),
+//       `
+//     )
 //   );
 
 //   return insertedCustomers;
@@ -94,29 +96,24 @@
 //         INSERT INTO revenue (month, revenue)
 //         VALUES (${rev.month}, ${rev.revenue})
 //         ON CONFLICT (month) DO NOTHING;
-//       `,
-//     ),
+//       `
+//     )
 //   );
 
 //   return insertedRevenue;
 // }
 
-export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
-  // try {
-  //   await client.sql`BEGIN`;
-  //   await seedUsers();
-  //   await seedCustomers();
-  //   await seedInvoices();
-  //   await seedRevenue();
-  //   await client.sql`COMMIT`;
-
-  //   return Response.json({ message: 'Database seeded successfully' });
-  // } catch (error) {
-  //   await client.sql`ROLLBACK`;
-  //   return Response.json({ error }, { status: 500 });
-  // }
-}
+// export async function GET() {
+//   try {
+//     await client.sql`BEGIN`;
+//     await seedUsers();
+//     await seedCustomers();
+//     await seedInvoices();
+//     await seedRevenue();
+//     await client.sql`COMMIT`;
+//     return Response.json({ message: "Database seeded successfully" });
+//   } catch (error) {
+//     await client.sql`ROLLBACK`;
+//     return Response.json({ error }, { status: 500 });
+//   }
+// }
